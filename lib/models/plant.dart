a9f812acd0b4f94d4293ff020f8155c72e1ff050
bungearnss <love2554f@hotmail.com>
2022-12-05 11:15:19 +0700

    add some model for detail screen

--- lib/models/plant.dart
+++ lib/models/plant.dart
@@ -1,15 +1,25 @@
 class Plant {
   String title;
   String plantType;
+  String description;
   int rate;
   int price;
   String imgUrl;
+  int light;
+  int moisture;
+  int temp;
+  int water;
 
   Plant({
     required this.title,
     required this.plantType,
+    required this.description,
     required this.rate,
     required this.price,
     required this.imgUrl,
+    required this.light,
+    required this.moisture,
+    required this.temp,
+    required this.water,
   });
 }
