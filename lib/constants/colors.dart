b0577024ead4d7d0d4f1a8a4f606b96ff5a30d66
bungearnss <love2554f@hotmail.com>
2022-12-05 16:05:40 +0700

    add some color for detail screen

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -5,3 +5,10 @@ Color secondaryColor = const Color.fromARGB(255, 240, 228, 158);
 
 Color mainThemeColor = const Color.fromRGBO(218, 229, 218, .8);
 Color accentColor = const Color.fromRGBO(89, 204, 106, 1);
+
+Color? backColor = Colors.grey[800];
+Color txtColor = const Color.fromRGBO(136, 143, 165, 1);
+
+Color greenColor = const Color.fromRGBO(202, 218, 180, 1);
+Color purpleColor = const Color.fromRGBO(180, 186, 218, 1);
+Color pinkColor = const Color.fromRGBO(232, 184, 186, 1);
